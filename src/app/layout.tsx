5200830e18ea1bcc70733b1fc6096a18d02f206d
taehoinnodep <147114106+taehoinnodep@users.noreply.github.com>
2025-07-08 10:56:24 +0900

    merge: from feat/survey [load google font]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,11 @@ import './globals.css';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import type { Metadata } from 'next';
-import { Geist } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'NamBTI : 남비티아이',
   description: '남이 해주는 MBTI',
 };
-
-const geist = Geist({
-  subsets: ['latin'],
-});
 
 export default function RootLayout({
   children,
@@ -20,11 +15,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex flex-col min-h-screen">
-          {/* <Header /> */}
-          <main className="flex-grow p-4">{children}</main>
-          {/* <Footer /> */}
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Asta+Sans:wght@300..800&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="w-screen h-screen">
+        <div className="flex flex-col w-full h-full bg-secondary">
+          <Header />
+          <main className="w-full h-full p-4">{children}</main>
+          <Footer />
         </div>
         {children}
       </body>
