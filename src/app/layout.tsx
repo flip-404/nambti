2ff000c2dd8ec0b9fdefe72bab7f55cd47ef85a4
na_0_i <nayoi1oo5@gmail.com>
2025-07-08 22:27:45 +0900

    feat: intro page에 validation 로직과 카카오 공유하기 기능 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import KakaoScript from '@/components/KakaoScript';
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         </div>
         {children}
       </body>
+      <KakaoScript />
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
   const h3Style = 'text-lg font-bold';
@@ -9,42 +10,79 @@ export default function Home() {
   const [contentsIdx, setContentsIdx] = useState(0);
   const [nickName, setNickName] = useState('');
   const [mbti, setMbti] = useState(['', '', '', '']);
+  const [userURL, setUserURL] = useState('');
 
   const moveNextPage = () => {
     setContentsIdx(contentsIdx + 1);
   };
 
   const handleKeyDownNickNameInput = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') setContentsIdx(contentsIdx + 1);
+    if (e.key === 'Enter') {
+      if (nickName.length === 0) return;
+      else setContentsIdx(contentsIdx + 1);
+    }
   };
 
   const handleChangeMbtiInput = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
     const newMBTI = [...mbti];
-    newMBTI[idx] = e.target.value;
+    const input = e.target.value.toUpperCase();
+
+    if (input.length > 1) return;
+    newMBTI[idx] = input;
 
     setMbti(newMBTI);
   };
 
+  const handleClickMbtiSubmit = () => {
+    if (mbti.some((value) => !value)) return;
+    if (mbti.some((_, idx) => !isValidMbti(idx))) return;
+
+    moveNextPage();
+  };
+
   const handleClickMbtiInputJump = () => {
-    setContentsIdx(contentsIdx + 1);
     setMbti(['', '', '', '']);
+    moveNextPage();
   };
 
   const handleClickCopyLink = () => {
-    navigator.clipboard
-      .writeText('http://www.nambiti.com/abcd1234dfgh5678')
-      .then(() => alert('복사 성공!')); // 토스트 open
+    navigator.clipboard.writeText(userURL).then(() => alert('복사 성공!'));
   };
 
-  const handleClickShare = () => {};
+  const handleClickShare = () => {
+    const { Kakao, location } = window;
+    Kakao.Share.sendScrap({
+      requestUrl: location.href,
+    });
+  };
+
+  const isValidMbti = (idx: number) => {
+    if (idx === 0 && (mbti[idx] === 'E' || mbti[idx] === 'I')) return true;
+    else if (idx === 1 && (mbti[idx] === 'S' || mbti[idx] === 'N')) return true;
+    else if (idx === 2 && (mbti[idx] === 'F' || mbti[idx] === 'T')) return true;
+    else if (idx === 3 && (mbti[idx] === 'J' || mbti[idx] === 'P')) return true;
+    else return false;
+  };
+
+  const generateUserURL = () => {
+    const prefix = 'www.nambiti.com/';
+    const uniqueId = uuidv4();
+    setUserURL(`${prefix}${uniqueId}`);
+  };
+
+  useEffect(() => {
+    if (contentsIdx === 3) {
+      generateUserURL();
+    }
+  }, [contentsIdx]);
 
   const contents = [
     {
       idx: 0,
       descEl: (
         <>
-          <h1 className="text-2xl font-bold">Nambti</h1>
-          <p>남비티아이가 뭔지 대충 설명 어쩌고 저쩌고</p>
+          <h1 className="text-2xl font-bold">남비티아이</h1>
+          <p>친구들이 보는 내 MBTI는 과연?</p>
         </>
       ),
       actionEl: (
@@ -71,10 +109,12 @@ export default function Home() {
       actionEl: (
         <div className="flex flex-col items-center gap-2">
           <div className="flex gap-2">
-            {Array.from({ length: 4 }).map((_, idx) => (
+            {Array.from({ length: 4 }, (_, idx) => (
               <input
                 key={idx}
-                className="w-10 h-10 rounded border-1 bg-sky-100 border-sky-500"
+                value={mbti[idx]}
+                tabIndex={idx}
+                className={`w-10 h-10 rounded border-1 text-center ${isValidMbti(idx) ? 'bg-sky-100 border-sky-500' : 'bg-red-100 border-red-500'}`}
                 onChange={(e) => handleChangeMbtiInput(e, idx)}
               />
             ))}
@@ -82,8 +122,8 @@ export default function Home() {
           <button className={buttonStyle} onClick={handleClickMbtiInputJump}>
             건너뛰기
           </button>
-          <button className={buttonStyle} onClick={moveNextPage}>
-            확인
+          <button className={buttonStyle} onClick={handleClickMbtiSubmit}>
+            입력완료
           </button>
         </div>
       ),
@@ -93,8 +133,8 @@ export default function Home() {
       descEl: <h3 className={h3Style}>친구들에게 링크를 공유해주세요.</h3>,
       actionEl: (
         <div className="flex flex-col items-center">
-          <div>www.nambiti.com/abcd1234efgh5678</div>
-          <div className="flex gap-1">
+          <div className="w-100 text-center text-xs pb-5">{userURL}</div>
+          <div className="flex gap-3">
             <button className={buttonStyle} onClick={handleClickCopyLink}>
               클립보드로 복사
             </button>
